7b18adc92ba4dd8781e422ac69dbb93bf1edc721
Cedric Turbe <cedricturbe@hotmail.com>
2023-02-28 20:13:46 +0100

    Routes, middleware, controllers, tout

--- backend/app.js
+++ backend/app.js
@@ -2,21 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require("body-parser");
-const Sauce = require('./models/Sauce');
-const User = require('./models/User');
-
-const app = express();
-// Utilisation de CORS pour éviter les erreurs de sécurité
-app.use(cors());
-// Middleware pour récupérer les données JSON envoyées dans le corps de la requête avec une limite de taille de 10kb
-app.use(bodyParser.json({ limit: '10kb' }));
-
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//     next();
-// });
+const saucesRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+const path = require('path');
 
 require('dotenv').config();
 const dbUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_HOST}/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`;
@@ -27,18 +15,23 @@ mongoose.connect(`${dbUrl}`,
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+const app = express();
+// Utilisation de CORS pour éviter les erreurs de sécurité
+app.use(cors());
+// Middleware pour récupérer les données JSON envoyées dans le corps de la requête avec une limite de taille de 10kb
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     next();
+// });
+
+app.use(bodyParser.json({ limit: '10kb' }));
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
