50663618e008e389839b3e07467b20724da1bb19
Cedric Turbe <cedricturbe@hotmail.com>
2023-02-28 15:11:17 +0100

    Création des schémas de modèles User et Sauce

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require("body-parser");
+const Sauce = require('./models/Sauce');
+const User = require('./models/User');
 
 const app = express();
 // Utilisation de CORS pour éviter les erreurs de sécurité
