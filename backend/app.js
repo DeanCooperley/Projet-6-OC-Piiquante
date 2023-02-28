a525a3693769f24aa342c4e59fb9c3e895bdb4f3
Cedric Turbe <cedricturbe@hotmail.com>
2023-02-28 13:03:26 +0100

    Installation de CORS, body-parser et Mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require("body-parser");
 
 const app = express();
+// Utilisation de CORS pour éviter les erreurs de sécurité
+app.use(cors());
+// Middleware pour récupérer les données JSON envoyées dans le corps de la requête avec une limite de taille de 10kb
+app.use(bodyParser.json({ limit: '10kb' }));
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     next();
+// });
+
+require('dotenv').config();
+const dbUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_HOST}/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(`${dbUrl}`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
   res.status(201);
