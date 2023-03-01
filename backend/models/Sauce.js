bae4d11d7d14e3f31dc155820e5e61f503b7760d
Cedric Turbe <cedricturbe@hotmail.com>
2023-03-01 11:20:44 +0100

    Ajout fichier like + correction erreurs dans controllers

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -10,8 +10,8 @@ const sauceSchema = mongoose.Schema({
     heat: { type: Number },
     likes: { type: Number, default: 0 },
     dislikes: { type: Number, default: 0 },
-    userLiked: [String],
-    userDisliked: [String]
+    usersLiked: [{ type: String }],
+    usersDisliked: [{ type: String }]
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const sauceCtrl = require('../controllers/sauce');
 const auth = require('../middleware/auth');
+const likeCtrl = require('../controllers/like');
 const multer = require('../middleware/multer-config');
 const router = express.Router();
 
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
-router.post('/:id/like', auth, sauceCtrl.likeSauce);
+router.post('/:id/like',auth, likeCtrl.addLikeOrDislike);
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 
