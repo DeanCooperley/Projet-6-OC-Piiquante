d7ad17f3eee4e1ab82ceaf7908b9ac57cdc8691a
Cedric Turbe <cedricturbe@hotmail.com>
2023-03-03 12:25:12 +0100

    Dossier images mis dans .gitignore

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,10 @@ const xml2js = require('xml2js');
 
 const config = require('./config');
 
+// Chemin de connexion à la base de données MongoDB
 const dbUrl = `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PW}@${config.MONGO_HOST}/${config.MONGO_DBNAME}?retryWrites=true&w=majority`;
 
+// Connexion à la base de données MongoDB avec Mongoose
 mongoose.connect(`${dbUrl}`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -20,6 +22,7 @@ mongoose.connect(`${dbUrl}`,
 
 const app = express();
 
+// Configuration du CORS (Cross-Origin Resource Sharing)
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -39,7 +42,7 @@ app.post('/xml', (req, res) => {
   // Récupérer le corps de la requête
   let xml = '';
   req.on('data', chunk => {
-    xml += chunk.toString();
+    xml += chunk.toString(); // convertir le Buffer en chaîne
   });
   req.on('end', () => {
     // Parser le XML avec xml2js
@@ -59,9 +62,10 @@ app.post('/xml', (req, res) => {
 app.use(bodyParser.json({ limit: '10kb' }));
 
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
-app.use(mongoSanitize()); // Middleware pour protéger contre les injections NoSQL
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' })); // Middleware pour protéger contre les attaques de type Cross-Site-Scripting (XSS)
+app.use(mongoSanitize()); // Middleware pour protéger contre les injections NoSQL, JavaScript et HTML (Insertion de caractères spéciaux)
 
+// Routes
 app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
